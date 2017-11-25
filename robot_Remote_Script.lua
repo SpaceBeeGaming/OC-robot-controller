000b6813ef5f17f22a439b0417d4bfc0e67e8cf4
SpaceBeeGaming <space.bee.gaming@gmail.com>
2017-11-25 13:30:21 +0200

    Added script file support to robot_Remote_Script.lua

--- robot_Remote_Script.lua
+++ robot_Remote_Script.lua
@@ -1,25 +1,35 @@
-local file = io.open("/robot_Remote_Script.txt", "r")
-
-local script = {}
-if file then
-  for line in file:lines() do
-    print(line)
-    table.insert(script, line)
-  end
-  file:close()
-end
-
 local event = require("event")
+local args = { ... }
 
-local script_built = {}
+if args[1] == nil then
+  print("Usage: robot_Remmote_Script <script>")
+  print("Scripts must be placed in '/scripts' folder")
+  print("Available scripts: ")
+  os.execute("ls /scripts")
+else
+  local file = io.open("/scripts/" .. args[1], "r")
 
-for step, command in ipairs(script) do
-  script_built[step] = "robot_Remote " .. command
-  print("Parsing: " .. script_built[step])
-end
+  local script = {}
+  if file then
+    for line in file:lines() do table.insert(script, line) end
+    file:close()
 
-for step = 1, #script_built do
-  os.execute(script_built[step])
-  local _, _, _, _, _, response = event.pull("modem_message")
-  print(response)
+    local script_built = {}
+
+    for step, command in ipairs(script) do
+      script_built[step] = "robot_Remote " .. command
+      print("Parsing: " .. script_built[step])
+    end
+
+    for step = 1, #script_built do
+      os.execute(script_built[step])
+      print("Executing: " .. script_built[step])
+      local _, _, _, _, _, response = event.pull("modem_message")
+      print("Responce: " .. response)
+    end
+  else
+    print("Script not found: place your script in '/scripts' folder")
+    print("Available scripts: ")
+    os.execute("ls /scripts")
+  end
 end
